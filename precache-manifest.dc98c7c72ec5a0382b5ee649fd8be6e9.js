b64f8ae673f17b4bcfe578f546a9a1c3910df9c2
itsvick <vivek_kasture@techjoomla.com>
2022-01-29 11:06:40 +0000

    Deploying to gh-pages from  @ d18a053740ba5a7ea157b8d210e86b9e2eade7b7 🚀

---
+++ precache-manifest.dc98c7c72ec5a0382b5ee649fd8be6e9.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a84f453bf17a2646e8f5f06bdca017c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c13d54de00d361cca0da",
+    "url": "/static/css/3.00373544.chunk.css"
+  },
+  {
+    "revision": "25dd6649159b6f9a01a2",
+    "url": "/static/css/4.3e7d09e6.chunk.css"
+  },
+  {
+    "revision": "290ccfb03ef09628c127",
+    "url": "/static/css/main.ea53a5d1.chunk.css"
+  },
+  {
+    "revision": "5a522edf41352fd4c9fb",
+    "url": "/static/js/2.f2e35554.chunk.js"
+  },
+  {
+    "revision": "0a6d64cd3b49be6a14afdf21fa7c6d43",
+    "url": "/static/js/2.f2e35554.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c13d54de00d361cca0da",
+    "url": "/static/js/3.525e104b.chunk.js"
+  },
+  {
+    "revision": "25dd6649159b6f9a01a2",
+    "url": "/static/js/4.f2717f9a.chunk.js"
+  },
+  {
+    "revision": "290ccfb03ef09628c127",
+    "url": "/static/js/main.3c6d4b68.chunk.js"
+  },
+  {
+    "revision": "812ec9343d1b46ab457a",
+    "url": "/static/js/runtime-main.fe847b6d.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "ec73ed7e39b57ab066eab9e0c03f7fe3",
+    "url": "/static/media/blogApp-logo.ec73ed7e.webp"
+  },
+  {
+    "revision": "dee18b77a65ab7ac42df699944cb7204",
+    "url": "/static/media/bni-logo.dee18b77.webp"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "8aab9417b09f02a49e466b4a143a3dfe",
+    "url": "/static/media/diksha-logo.8aab9417.svg"
+  },
+  {
+    "revision": "bbd205145d18c228d6bc2de0b78b63a0",
+    "url": "/static/media/easysocial.bbd20514.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "5e3aaea4105b4e835cfa9841c925e3f9",
+    "url": "/static/media/suk-logo.5e3aaea4.png"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  },
+  {
+    "revision": "3049e14501ee94579548831474cb2b28",
+    "url": "/static/media/tekdi.3049e145.png"
+  }
+]);
